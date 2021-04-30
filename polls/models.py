4a13785079319d6b2aacdba8927042ac32973d7f
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-04-30 15:15:21 +0700

    Edit model for question, choice and vote

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes_score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice,related_name= 'votes', on_delete=models.CASCADE)
+    time = models.DateTimeField('time', blank=True,default=timezone.now)
