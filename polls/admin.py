84b92d4710f982804c32c5556f2155742a321a84
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-04-30 15:50:46 +0700

    Edit admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question,Choice,Vote
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Vote)
--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/detail', views.index, name='detail),
 ]
